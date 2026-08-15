ce33454c954503a2a5402469ea09956b0d1d3f48
Damian Castro <damianalbertocastro@gmail.com>
2026-08-15 15:30:45 -0400

    commit, and synchronize your local repository with the remote repository. save the changes

--- apps/admin-portal/app/layout.tsx
+++ apps/admin-portal/app/layout.tsx
@@ -1,8 +1,11 @@
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
-
+const inter = localFont({
+  src: "./fonts/InterVariable.woff2",
+  display: "swap",
+  weight: "100 900",
+});
 
 export default function RootLayout({
   children,
